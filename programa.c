f3ac46b8f2c92c3ff57f50d0ef3161b5ea16be12
daniramonllull <55685670+daniramonllull@users.noreply.github.com>
2020-02-03 09:49:24 +0100

    Modificar darvueltas y do while

--- programa.c
+++ programa.c
@@ -1,12 +1,21 @@
 void mover_tiempo (int tiempo, int velocidad, char sentido);
 void darvueltas(int tiempo, int velocidad, char sentido);
-
-
+/*
+ Salida C: Rueda izquierda
+ Salida B: Rueda derecha
+*/
 task main(){
 
     //mover_tiempo(1000,75,'D');
     //mover_tiempo(1000,75,'A');
-    darvueltas(2000,75,'H');
+
+    int velocidad = 5;
+    do{
+        darvueltas(500,velocidad,'H');
+        //darvueltas(1000,75,'A');
+        velocidad = velocidad + 5;        
+    } while (velocidad <= 100);
+   
 
 }
 
@@ -24,9 +33,19 @@ void mover_tiempo (int tiempo, int velocidad, char sentido){
 }
 
 void darvueltas(int tiempo, int velocidad, char sentido) {
+    
+    if(sentido == 'H'){
+        //sentido horario (H)
+        OnFwd(OUT_C,velocidad);
+        OnRev(OUT_B,velocidad);
+        Wait(tiempo);
+    }
+    else {
+        //sentido antihorario(A)
+        OnFwd(OUT_B,velocidad);
+        OnRev(OUT_C,velocidad);
+        Wait(tiempo);
+    }
 
-    OnFwd(OUT_C,velocidad);
-    OnRev(OUT_B,velocidad);
-    Wait(tiempo);
 
 }
